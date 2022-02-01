7343d2f485950339cd5a7d73b2f2606b19253229
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2022-02-01 13:11:28 +0530

    Update postfixevaluation.c

--- postfixevaluation.c
+++ postfixevaluation.c
@@ -1,79 +1,76 @@
 #include<stdio.h>
-#define STACK_MAX 100
+#define stm 100
 
-int arr[STACK_MAX];
+int arr[stm];
 int top=-1;
 
-void push(int element)
-{
-     if(top>STACK_MAX-1){
-        printf("Stack Overflow");
-        return;
-     }
-
-     else
-     {top++;
-      arr[top]=element;}
-}
-int pop()
-{
-      if(top==-1)
-      {
-        printf("Stack Underflow");
-        return 0;
-      }
-     else
-      {
-        int element;
-        element=arr[top];
-        top--;
-        return (element);
-      }
-}
-
-int main()
-{
-    char postfix[STACK_MAX];
-    int i,a,b,res,num;
-
-    printf("Enter the postfix expression:");
-    scanf("%s",postfix);
-
-    for(i=0;postfix[i]!='\0';i++)
+ void push(int e)
+   {
+    if(top>stm-1)
      {
-         switch(postfix[i])
-          {
-                  case'+':a=pop();
-                          b=pop();
-                          res=a+b;
-                          push(res);
-                          break;
-
-                  case'-':a=pop();
-                          b=pop();
-                          res=a-b;
-                          push(res);
-                          break;
-
-                  case'*':a=pop();
-                          b=pop();
-                          res=a*b;
-                          push(res);
-                          break;
-
-                  case'/':a=pop();
-                          b=pop();
-                          res=a/b;
-                          push(res);
-                          break;
-
-                 default:
-                         printf("Enter the value of %c:",postfix[i]);
-                         scanf("%d",&num);
-                         push(num);
-                         break;
-             }
+      printf("Stack Overflow");
+      return;
+     }
+    else
+     {
+      top++;
+      arr[top]=e;
       }
-  printf(" The postfix expression is: %s\n",postfix);
-  printf(" Result after evaluation is :%d",arr[top]);
- }
+   }
+     
+ int pop()
+   {
+    if(top==-1)
+      {
+       printf("Stack Underflow");
+       return 0;
+      }
+    else
+      {
+       int e;
+       e=arr[top];
+       top--;
+       return (e);
+      }
+   } 
+
+ int main()
+   {
+    char postfix[stm];
+    int i,a,b,res,num;
+    printf("Enter the postfix expression for sloving:");
+    scanf("%s",postfix);
+    for(i=0;postfix[i]!='\0';i++)
+      {
+       switch(postfix[i])
+         {
+          case'+':a=pop();
+          b=pop();
+          res=a+b;
+          push(res);
+          break;
+          case'-':a=pop();
+          b=pop();
+          res=a-b;
+          push(res);
+          break;
+          case'*':a=pop();
+          b=pop();
+          res=a*b;
+          push(res);
+          break;
+          case'/':a=pop();
+          b=pop();
+          res=a/b;
+          push(res);
+          break;
+          default:
+          printf("Enter the value of %c:",postfix[i]);
+          scanf("%d",&num);
+          push(num);
+          break;
+         }
+      }
+   printf(" The postfix expression is: %s\n",postfix);
+   printf(" Result:%d",arr[top]);
+  }
